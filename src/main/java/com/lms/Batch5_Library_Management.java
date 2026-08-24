9aa4d00880bbc268b70b8ddbbb25b762322bb50e
YuSandy369 <yusandy@mail.arakkha.net>
2026-08-24 16:05:39 +0200

    no message

--- src/main/java/com/lms/Batch5_Library_Management.java
+++ src/main/java/com/lms/Batch5_Library_Management.java
@@ -12,6 +12,5 @@ public class Batch5_Library_Management {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(" Hi Guys");
     }
 }
