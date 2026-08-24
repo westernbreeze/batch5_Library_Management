b9ab255eaa42ed5427d120cef4dc113159ea361e
shweousan <shweousann@gmail.com>
2026-08-24 14:41:53 -0800

    this is my first commit

---
+++ src/main/java/com/lms/Batch5_Library_Management.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lms;
+
+/**
+ *
+ * @author Admin
+ */
+public class Batch5_Library_Management {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
