54df81c2dddf7fb38ca9fb4b1c50782f75bc2a42
shweousan <shweousann@gmail.com>
2026-08-24 15:44:16 -0800

    Khant Naing Tun

--- Library/src/main/java/com/mycompany/library/Library.java
+++ Library/src/main/java/com/mycompany/library/Library.java
@@ -12,5 +12,7 @@ public class Library {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Khant Chay");
+        System.out.println("Ma Ma Win");
     }
 }
