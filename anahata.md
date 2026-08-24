41961a63ffa7babf91edc312a49d8393c33fa988
U Myo Maung <myomaung@mail.arakkha.net>
2026-08-24 19:01:57 +0630

    updated library management and added anahata file

---
+++ anahata.md
@@ -0,0 +1,3 @@
+# Anahata Project Notes
+
+This file is for Anahata AI Assistant's notes regarding the 'batch5_Library_Management' project.
--- src/main/java/com/lms/Batch5_Library_Management.java
+++ src/main/java/com/lms/Batch5_Library_Management.java
@@ -12,5 +12,7 @@ public class Batch5_Library_Management {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        System.out.println("World Hello" + "Testing");
     }
 }
