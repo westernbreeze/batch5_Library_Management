9bf397a51fe36963a073357969bf05b008fd2ef5
U Myo Maung <myomaung@mail.arakkha.net>
2026-08-25 22:40:15 +0630

    Login testing code

--- src/main/java/com/lms/ui/Login.java
+++ src/main/java/com/lms/ui/Login.java
@@ -9,5 +9,24 @@ package com.lms.ui;
  * @author Admin
  */
 public class Login {
+    public int addition(){
+        int a = 0;
+        int b= 2;
+        int result = a + b;
+        return result;
+    }
     
+    public int substration(int a, int b) {
+        return a-b;
+    }
+    
+    public static void main(String []args) {
+        Login add = new Login();
+        Login sub = new Login();
+        
+        int result = add.addition();
+        int subResult = sub.substration(3, 0);
+        System.out.println("Addition Result" + result);
+        System.out.println("Substration Result" + subResult);
+    }
 }
